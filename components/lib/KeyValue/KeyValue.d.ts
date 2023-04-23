6e0bae1512f3b1430b3523cbc0a96946dc761312
Noah Overcash <ncovercash@ua.edu>
2023-04-23 00:55:14 -0500

    Add docblocks

--- components/lib/KeyValue/KeyValue.d.ts
+++ components/lib/KeyValue/KeyValue.d.ts
@@ -2,9 +2,29 @@ import { FunctionComponent, ReactNode } from 'react';
 import { RequireExactlyOne } from 'type-fest';
 
 export type KeyValueProps = {
+  /** The label for the key/value pair */
   label: ReactNode;
+  /** A value to show below the key */
   subValue?: ReactNode;
-} & RequireExactlyOne<{ children: ReactNode; value: ReactNode }>;
+} & RequireExactlyOne<{
+  /** The value to display */
+  children: ReactNode;
+  /** The value to display */
+  value: ReactNode;
+}>;
 
-export const KeyValue: FunctionComponent<KeyValueProps>;
+/**
+ * Renders a given key/value pair
+ *
+ * @example
+ * <KeyValue
+ *   label="Some label"
+ *   value="Some value"
+ *   subValue="Some sub value"
+ * />
+ * @example
+ * <KeyValue label="Some label">
+ *   <span data-test-id="my-test-string">Some node</span>
+ * </KeyValue>
+ */
 export default KeyValue;
--- components/lib/Label/Label.d.ts
+++ components/lib/Label/Label.d.ts
@@ -1,13 +1,32 @@
 import { FunctionComponent, ReactNode } from 'react';
 
 export interface LabelProps {
+  /** The content of the `<Label>` */
   children: ReactNode;
+  /** Applies a custom class name */
   className?: string;
+  /** Adds a `for` attribute on the label-element */
+  htmlFor?: string;
+  /** Adds an ID on the label-element */
   id?: string;
+  /** Renders a lock-icon next to the label (indicating that the associated field is read-only) */
   readOnly?: boolean;
+  /** Renders an asterisk next to the label (indicating that the associated field is required) */
   required?: boolean;
+  /** Change the tag name of the component (`label` by default) */
   tagName?: string;
 }
 
-export const Label: FunctionComponent<LabelProps>;
+/**
+ * Renders a simple <label> with a few optional additions
+ *
+ * Note: Most form components have already labels implemented directly, activated
+ * by the `label`-prop. This component is only relevant in special circumstances
+ * – e.g. when creating a new form component.
+ *
+ * @example
+ * <Label required>
+ *   My label
+ * </Label>
+ */
 export default Label;
