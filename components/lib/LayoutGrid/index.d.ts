504d9917958755114250c6588db09631e733e213
Noah Overcash <ncovercash@ua.edu>
2023-05-17 14:48:24 -0400

    Add layout grid components (#19)
    * Add accordion typings
    
    * Add layout grid components

--- components/lib/LayoutGrid/index.d.ts
+++ components/lib/LayoutGrid/index.d.ts
@@ -1,5 +1,21 @@
-export const Grid: any;
-export const Row: any;
-export const getRowProps: any;
-export const Col: any;
-export const getColumnProps: any;
+/* eslint-disable max-classes-per-file */
+import { Component, ReactNode } from 'react';
+import { GridProps, RowProps, ColProps } from 'react-flexbox-grid';
+
+export class Grid extends Component<
+  GridProps & {
+    children?: ReactNode;
+  }
+> {}
+
+export class Row extends Component<
+  RowProps & {
+    children?: ReactNode;
+  }
+> {}
+
+export class Col extends Component<
+  ColProps & {
+    children?: ReactNode;
+  }
+> {}
