69be7c6a21826abfa14925df4b3ed52f66f91ce7
Noah Overcash <novercash@ebsco.com>
2023-06-01 14:07:27 -0500

    Allow react nodes as option labels

--- components/lib/Select/Select.d.ts
+++ components/lib/Select/Select.d.ts
@@ -5,7 +5,7 @@ import { RequireExactlyOne } from 'type-fest';
  * The default option type to be used, contains a label, potentially a value, and an option to disable
  */
 export interface OptionType<ValueType = never> {
-  label: string;
+  label: ReactNode;
   value: ValueType;
   disabled?: boolean;
 }
