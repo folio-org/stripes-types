e297d54a67686df0d4ee3f9086204b51c657fa6c
Noah Overcash <novercash@ebsco.com>
2023-10-09 16:04:54 -0400

    Fix hasPerm, add hasInterface

--- core/src/Stripes.d.ts
+++ core/src/Stripes.d.ts
@@ -4,9 +4,28 @@ import { Requireable } from 'react';
 export interface StripesType {
   /**
    * Check if the current user has a given permission
+   *
+   * @returns undefined, if the user is not logged in or similar error
+   * @returns true, if the user has the permission
+   * @returns false, if the user does not have the permission
    * @see https://github.com/folio-org/stripes/blob/master/doc/dev-guide.md#testing-for-permission
    */
-  hasPerm(perm: string): boolean;
+  hasPerm(perm: string): boolean | undefined;
+
+  /**
+   * Check if an interface is available
+   *
+   * @returns undefined, on error
+   * @returns false, if the interface is not present
+   * @returns true, if the interface is available and no specific version was requested
+   * @returns the version string, if the interface and requested version is available
+   * @returns 0, if the interface is available but the requested version is not
+   * @see https://github.com/folio-org/stripes/blob/master/doc/dev-guide.md#testing-for-interfaces
+   */
+  hasInterface(
+    interface: string,
+    version?: string,
+  ): boolean | string | undefined;
 
   // TODO: fill this in with additional properties from Stripes object
   // https://github.com/folio-org/stripes/blob/master/doc/dev-guide.md#the-stripes-object
@@ -16,6 +35,11 @@ export interface StripesType {
 export const stripesShape: Requireable<StripesType>;
 
 export class Stripes implements StripesType {
-  hasPerm(perm: string): boolean;
+  hasPerm(perm: string): boolean | undefined;
+  hasInterface(
+    interface: string,
+    version?: string,
+  ): boolean | string | undefined;
+
   [key: string]: unknown;
 }
