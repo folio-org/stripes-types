5e7b774291312b705a5474c2d0a59ed95c75709e
Zak Burke <zburke@ebsco.com>
2023-10-13 09:38:24 -0400

    STRIPES-893 align LayoutGrid with stripes-components (#37)
    `<LayoutGrid>` types were still based on the old `react-flexbox-grid`
    implementation instead of the current local implementation.
    
    Refs STRIPES-893

---
+++ components/lib/LayoutGrid/Col.d.ts
@@ -0,0 +1,28 @@
+import { FunctionComponent, ReactNode } from 'react';
+
+import { ColumnSize } from './ColumnSize';
+import { ViewportSize } from './ViewportSize';
+
+export interface ColProps {
+  xs?: ColumnSize,
+  sm?: ColumnSize,
+  md?: ColumnSize,
+  lg?: ColumnSize,
+  xl?: ColumnSize,
+  xsOffset?: number,
+  smOffset?: number,
+  mdOffset?: number,
+  lgOffset?: number,
+  xlOffset?: number,
+  first?: ViewportSize,
+  last?: ViewportSize,
+  /** Applies a custom class name */
+  className?: string,
+  /** Change the tag name of the component (`div` by default) */
+  tagName?: string,
+  /** The content of the `<Col>` */
+  children?: ReactNode,
+}
+
+declare const Col: FunctionComponent<ColProps>;
+export default Col;
---
+++ components/lib/LayoutGrid/Row.d.ts
@@ -0,0 +1,23 @@
+import { FunctionComponent, ReactNode } from 'react';
+import { ViewportSize } from './ViewportSize';
+
+export interface RowProps {
+  reverse?: boolean,
+  start?: ViewportSize,
+  center?: ViewportSize,
+  end?: ViewportSize,
+  top?: ViewportSize,
+  middle?: ViewportSize,
+  bottom?: ViewportSize,
+  around?: ViewportSize,
+  between?: ViewportSize,
+  /** Applies a custom class name */
+  className?: string,
+  /** Change the tag name of the component (`div` by default) */
+  tagName?: string,
+  /** The content of the `<Row>` */
+  children?: ReactNode
+}
+
+declare const Row: FunctionComponent<RowProps>;
+export default Row;
