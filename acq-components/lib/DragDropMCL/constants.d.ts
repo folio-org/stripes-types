ba2e5b6ff5aff5a0e45c964f7374d19d190cb80f
Noah Overcash <novercash@ebsco.com>
2023-12-21 17:08:39 -0500

    cleanup

--- acq-components/lib/DragDropMCL/constants.d.ts
+++ acq-components/lib/DragDropMCL/constants.d.ts
@@ -1,13 +1,3 @@
-export namespace defaultColumnWidths {
-  namespace draggable {
-    let max: number;
-  }
-}
-export namespace defaultColumnMapping {
-  let draggable_1: string;
-  export { draggable_1 as draggable };
-}
-export namespace defaultFormatter {
-  export const draggable_2: any;
-  export { draggable_2 as draggable };
-}
+export const defaultColumnWidths : any;
+export const defaultColumnMapping : any;
+export const defaultFormatter: any;
