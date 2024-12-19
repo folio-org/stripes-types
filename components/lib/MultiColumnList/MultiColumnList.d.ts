2cf13168daee216797c6bb7b8e1b37c24c58ce02
John Coburn <jcoburn@ebsco.com>
2024-12-19 10:32:31 -0600

    Minor lint

--- components/lib/MultiColumnList/MultiColumnList.d.ts
+++ components/lib/MultiColumnList/MultiColumnList.d.ts
@@ -154,7 +154,7 @@ export type MultiColumnListPagingTypes =
       pagingCanGoNext?: boolean;
       pagingCanGoPrevious?: boolean;
       hidePageIndices?: boolean;
-      /** optional prop for manually controlling the page numbers indicated on the prev/next controls **/
+      /** optional prop for manually controlling the page numbers indicated on the prev/next controls */
       pagingOffset? : number;
     }
   | {
